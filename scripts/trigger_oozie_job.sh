475af4a44006c97d82edcaa2f4875a46c0b239a1
Tom Cooling <Tom.Cooling@ons.gov.uk>
2018-08-24 10:07:38 +0100

    Add passing of bi-ENV-ci password to oozie script
    - Add backslash before special chars in password
    - Use password in oozie script for kinit command

--- scripts/trigger_oozie_job.sh
+++ scripts/trigger_oozie_job.sh
@@ -5,13 +5,14 @@
 # in a populated ElasticSearch index.
 
 SCRIPT_NAME=$0
-REQUIRED_NUM_ARGS=4
+REQUIRED_NUM_ARGS=5
 
 # Fail fast if we get any errors
 set -e
 
 usage() {
     echo "usage: ${SCRIPT_NAME} env host oozie_home"
+    echo "  password                password for bi-ENV-ci"
     echo "  env                     environment dev/test/beta"
     echo "  host                    ssh target host"
     echo "  oozie_home              oozie home url"
@@ -25,10 +26,11 @@ if [ $# -ne $REQUIRED_NUM_ARGS ] ; then
     usage
 fi
 
-ENV=$1
-HOST=$2
-OOZIE_HOME=$3
-INDEX_NAME=$4
+PASSWORD=$1
+ENV=$2
+HOST=$3
+OOZIE_HOME=$4
+INDEX_NAME=$5
 
 # Create the directory for our job.properties file and replace the INDEX_NAME value, then send it using scp
 ssh bi-${ENV}-ci@${HOST} "mkdir -p bi-${ENV}-ingestion-parquet"
@@ -37,11 +39,19 @@ mv ./configuration/${ENV}/updated_job.properties ./configuration/${ENV}/job.prop
 scp ./configuration/${ENV}/job.properties bi-${ENV}-ci@${HOST}:bi-${ENV}-ingestion-parquet
 echo "Successfully transfered ./configuration/${ENV}/job.properties to bi-${ENV}-ci@${HOST}:bi-${ENV}-ingestion-parquet"
 
+# Replace special characters
+REPLACED_PASSWORD=$(echo ''"$PASSWORD"'' | awk '{gsub( /[(*|`$&)]/, "\\\\&"); print $0}')
+
 # Trigger the oozie job and get the job id, remove unused chars from the id and then poll it.
 # ENVIRONMENT and INDEX_NAME are exported in the ssh step so that the job.properties file
 # used by Oozie can use them
 # JOB_ID is something like 'job: 213871982-213123123-asdasd', we remove 'job: '
-ssh -tt bi-${ENV}-ci@${HOST} OOZIE_HOME=$OOZIE_HOME ENV=$ENV 'bash -s' << 'ENDSSH'
+ssh -tt bi-${ENV}-ci@${HOST} BI_PASSWORD=$REPLACED_PASSWORD OOZIE_HOME=$OOZIE_HOME ENV=$ENV 'bash -s' << 'ENDSSH'
+    set -e
+
+    # Refresh kerboros authentication
+    echo "${BI_PASSWORD}" | kinit
+
     TIMEOUT=1000
     INTERVAL=1
     OOZIE_ID_INDEX=5
